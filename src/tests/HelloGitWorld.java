aec13b2f21965150a6995c95cf61f2b5b3186d32
osmanolgun <osmanolgun.de@gmail.com>
2020-05-05 19:08:55 +0200

    only update

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -3,5 +3,6 @@ package tests;
 public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git World");
+        System.out.println("Hello Git World ben Osman");
     }
 }
--- src/tests/Login.java
+++ src/tests/Login.java
@@ -4,5 +4,6 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println("This is login class");
+        System.out.println("this is a change test for Git");
     }
 }
