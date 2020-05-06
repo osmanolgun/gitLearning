98a2cdfa38c5645be761e2b581a6da662e2e44e7
osmanolgun <osmanolgun.de@gmail.com>
2020-05-07 00:24:22 +0200

    intellije check butonudadanu deniyorum asdfasfasdf asdf asdfsfd

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -5,14 +5,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("This is login class");
         System.out.println("this is a change test for Git");
-        //buraya ekleme yaptım
-        //bir daha ekleme yaptım
-        //bu da son olsun
-        //asfsafsdf
-        //as
-
-        //son
-        // eee yetti beeee
-        //adasd
+        //yorumları sildim
     }
 }
