f6dfc2a690d35f40487c6815871cc9a1c6dc65c7
osmanolgun <62035481+osmanolgun@users.noreply.github.com>
2020-05-06 18:33:38 +0200

    Update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -5,5 +5,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("This is login class");
         System.out.println("this is a change test for Git");
+        //buraya ekleme yaptım
     }
 }
